c3842d4d14a28306cec974e027addbd24e93ced7
@CAYdenberg <ydenberg@gmail.com>
2018-08-25 20:39:03 -0600

    Write spec

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const popsicle = require('popsicle')
+
+export const popsicleRequest = ticket => {
+  return popsicle.request(ticket)
+    .use(popsicle.plugins.parse('json'))
+    .then(res => res.status < 400 ? res.body : Promise.reject(res.status))
+}
+
+export default function(doRequest = popsicleRequest) {
+  return () => next => action => {
+    if (action.popsicle) {
+      doRequest(action.popsicle).then(body => {
+        return next(action.response(body, action))
+      }).catch(status => {
+        return next({type: action.error || '@@POPSICLE/ERROR', status})
+      })
+    }
+
+    return next(action)
+  }
+}
